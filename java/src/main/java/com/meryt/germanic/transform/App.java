2bbd78a62345fe43d0e91682d1048e5618208f8e
meryt <meryt@meryt.com>
2012-12-17 17:43:48 -0800

    enable command-line usage

--- java/src/main/java/com/meryt/germanic/transform/App.java
+++ java/src/main/java/com/meryt/germanic/transform/App.java
@@ -1,6 +1,7 @@
 package com.meryt.germanic.transform;
 
-import org.apache.commons.cli.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Hello world!
@@ -8,40 +9,82 @@ import org.apache.commons.cli.*;
  */
 public class App 
 {
+	private static HashMap<String,String> validCommands = new HashMap<String,String>();
 	
     public static void main( String[] args )
     {
-    	// create the command line parser
-    	CommandLineParser parser = new PosixParser();
+    	validCommands.put("help", "display this message, or help <command> for help with a command");
+    	validCommands.put("character-mappings", "display the character mappings");
     	
-    	Options options = new Options();
-    	options.addOption("a", "all", false, "do all the things");
-    	options.addOption("h", "help", false, "show help");
-
-    	boolean helloToAll = false;
-    	try {
-    	    // parse the command line arguments
-    	    CommandLine line = parser.parse(options, args);
-
-    	    if (line.hasOption( "all" ) ) {
-    	        helloToAll = true;
-    	    }
-    	    if (line.hasOption("help") || 0 == args.length) {
-    	    	showHelp(options);
-    	    	return;
-    	    }
-    	}
-    	catch( ParseException exp ) {
-    	    System.out.println( "Unexpected exception:" + exp.getMessage() );
+    	if (0 == args.length) {
+    		showGlobalHelp();
+    		return;
     	}
     	
-    	String msg = helloToAll ? "Hello to all!" : "Hello World!";
-        System.out.println(msg);
+    	String command = args[0];
+    	if (isValidCommand(command)) {
+    		String[] remainingArgs = new String[args.length - 1];
+    		System.arraycopy(args, 1, remainingArgs, 0, args.length - 1);
+    		if ("help".equals(command)) {
+    			if (0 == remainingArgs.length) {
+    				showGlobalHelp();
+    				return;
+    			}
+    			String requestedCommand = remainingArgs[0];
+    			if (!isValidCommand(requestedCommand)) {
+    				System.err.println("Unrecognized command: " + requestedCommand);
+    				showGlobalHelp();
+    				return;
+    			}
+    			showCommandHelp(requestedCommand);
+    			return;
+    		}
+    		else if ("character-mappings".equals(command)) {
+    			CharacterDatabase.main(remainingArgs);
+    			return;
+    		}
+    	} else {
+    		showGlobalHelp();
+    		return;
+    	}
+    	
     }
     
-    private static void showHelp(Options options)
+    /**
+     * Determine whether the string is a valid top-level command
+     * @param command - a command, such as "help"
+     * @return boolean
+     */
+    private static boolean isValidCommand(String command)
     {
-    	HelpFormatter formatter = new HelpFormatter();
-    	formatter.printHelp("transform", options );
+    	return validCommands.containsKey(command);
     }
+    
+    /**
+     * Show help for a given command
+     */
+    private static void showCommandHelp(String command)
+    {
+    	if (command.equals("character-mappings")) {
+    		CharacterDatabase.showHelp();
+    		return;
+    	}
+    	System.out.println("Requested help for " + command);
+    }
+    
+    /**
+     * Show help for the app
+     */
+    private static void showGlobalHelp()
+    {
+    	System.out.println("usage: transform <command> <options>");
+    	System.out.println("  Valid commands are:");
+    	
+    	for (Map.Entry<String, String> entry : validCommands.entrySet()) {
+    	    String command = entry.getKey();
+    	    String description = entry.getValue();
+    	    System.out.println("    " + command + " - " + description);
+    	}    	
+    }
+    
 }
--- java/src/main/java/com/meryt/germanic/transform/CharacterDatabase.java
+++ java/src/main/java/com/meryt/germanic/transform/CharacterDatabase.java
@@ -8,7 +8,9 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.commons.cli.*;
 
 public class CharacterDatabase
 {
@@ -73,7 +75,7 @@ public class CharacterDatabase
 		
 		return tokens;
 	}
-
+	
 	/**
 	 * Get the lines.  Read from a file if they haven't been set.
 	 * 
@@ -114,6 +116,66 @@ public class CharacterDatabase
 		}
 		BufferedReader br = new BufferedReader(isr);
 		return br;
+	}
+
+	/**
+	 * Do something with the character database, depending on the args.
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+    	// create the command line parser
+    	CommandLineParser parser = new PosixParser();
+    	
+    	Options options = getOptions();
+    	
+    	String filePath = null;
+    	try {
+    	    // parse the command line arguments
+    		CommandLine commandline = parser.parse(options, args);
+    	    filePath = commandline.getOptionValue("file");
+    	    if (null == filePath || filePath.isEmpty()) {
+    	    	showHelp();
+    	    	System.exit(1);
+    	    }
+    	}
+    	catch( ParseException exp ) {
+    	    System.err.println("Unexpected exception:" + exp.getMessage());
+    	    System.exit(1);
+    	}
+
+    	CharacterDatabase chardb = new CharacterDatabase(filePath);
+    	try {
+			HashMap<String, String> mappings = chardb.getSearchReplaceTokens();
+			displayMappings(mappings);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+    	
+	}
+	
+	private static void displayMappings(HashMap<String,String> mappings)
+	{
+		for (Map.Entry<String, String> entry : mappings.entrySet()) {
+		    String entity = entry.getKey();
+		    String utf8Encoding = entry.getValue();
+		    System.out.println(entity + "\t" + utf8Encoding);
+		}		
+	}
+	
+	private static Options getOptions()
+	{
+    	Options options = new Options();
+    	options.addOption("f", "file", true, 
+    			"path to the file containing character mappings (hint: character_database.txt");
+    	return options;
+	}
+	
+	public static void showHelp()
+	{
+		HelpFormatter formatter = new HelpFormatter();
+    	formatter.printHelp("transform character-mappings", getOptions());
 	}
 	
 }
